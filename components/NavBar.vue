7c40a1bc25111c5f4c22a1aa7d4084834b1e0fb6
Irving Aguilar <irv.agui@gmail.com>
2023-05-20 14:32:12 -0600

    ✨default layout basic implementation

---
+++ components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-gray-700 w-full">
+    <nav class="px-5 py-5 flex justify-between text-white">
+      <ul class="flex">
+        <li>
+          Inicio
+        </li>
+      </ul>
+      <ul class="flex">
+        <li>
+          <NuxtLink to="/api/auth/signin">
+            Login
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-gray-800">
+    <NavBar />
+    <slot />
+  </div>
+</template>
